84d7774934530666dcadf2ab2e04c49bfd2e8edf
Andrei Balan <84906111+SWEET1S@users.noreply.github.com>
2023-07-17 14:17:58 +0300

    first commit

---
+++ ro/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Authentication Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used during authentication for various
+| messages that we need to display to the user. You are free to modify
+| these language lines according to your application's requirements.
+|
+*/
+
+return [
+    'failed' => 'Nume de utilizator sau parolă greșită.',
+    'password' => 'Parolă greșită.',
+    'throttle' => 'Prea multe încercări de conectare. Vă rugăm să încercați din nou peste :seconds secunde.',
+
+    'socialite' => [
+        'link_exists' => 'Contul este deja legat.',
+        'linked_success' => 'Contul a fost legat cu succes.',
+    ]
+];
---
+++ ro/ui.php
@@ -0,0 +1,88 @@
+<?php
+
+return [
+    'profile' => 'Profil',
+    'dashboard' => 'Tablou de bord',
+    'add' => 'Adăugare',
+    'create' => 'Creare',
+    'edit' => 'Editare',
+    'save' => 'Salvare',
+    'saved' => 'Salvat',
+    'saved_error' => 'Eroare la salvare',
+    'filters' => 'Filtre',
+    'search' => 'Căutare',
+    'reset' => 'Resetare',
+    'total' => 'Total',
+    'deleted' => 'Șters',
+    'delete' => 'Ștergere',
+    'deleting' => 'Se șterge',
+    'download' => 'Descărcare',
+    'export' => 'Export',
+    'cancel' => 'Anulare',
+    'confirm' => 'Confirmare',
+    'close' => 'Închidere',
+    'confirm_message' => 'Sigur doriți să ștergeți înregistrarea?',
+    'back' => 'Înapoi',
+    'more' => 'Mai mult',
+    'all_sections' => 'Toate secțiunile',
+    'file' => 'Fișier',
+    'user' => 'Utilizator',
+    'changes' => 'Modificări',
+    'last_changes' => 'Ultimele 5 modificări',
+    'date' => 'Data',
+    'field' => 'Câmp',
+    'before' => 'Înainte',
+    'after' => 'După',
+    'loading' => 'Se încarcă...',
+    'notfound' => 'Momentan nu există înregistrări',
+    'collapse_menu' => 'Restrângere meniu',
+    '404' => 'Houston! Avem o problemă - pagina nu a fost găsită',
+    'notifications' => [
+        'title' => 'Notificări',
+        'mark_as_read_all' => 'Marchează totul ca citit',
+        'mark_as_read' => 'Marchează ca citit',
+    ],
+    'login' => [
+        'title' => 'Bine ați venit la :moonshine_title!',
+        'description' => 'Vă rugăm să vă autentificați în contul dvs.',
+        'authorization' => 'Autorizare',
+        'remember_me' => 'Ține-mă minte',
+        'login' => 'Autentificare',
+        'logout' => 'Deconectare',
+        'username' => 'Nume utilizator',
+        'email' => 'E-mail',
+        'password' => 'Parolă',
+        'or_socials' => 'sau',
+    ],
+    'resource' => [
+        'system' => 'Sistem',
+        'role' => 'Roluri',
+        'name' => 'Nume',
+        'email' => 'E-mail',
+        'password' => 'Parolă',
+        'repeat_password' => 'Repetă parola',
+        'avatar' => 'Avatar',
+        'created_at' => 'Data creării',
+        'admins_title' => 'Administratori',
+        'role_title' => 'Roluri',
+        'role_name' => 'Nume rol',
+
+        'main_information' => 'Informații principale',
+        'change_password' => 'Schimbă parola',
+
+        'link_socialite' => 'Asociază conturi',
+        'linked_socialite' => 'Conturi asociază',
+
+        'queued' => 'În așteptare',
+
+        'export' => [
+            'exported' => 'Exportat',
+        ],
+
+        'import' => [
+            'imported' => 'Importat',
+            'file_required' => 'Fișierul este obligatoriu',
+            'extension_not_supported' => 'Extensia nu este acceptată'
+        ]
+    ]
+];
